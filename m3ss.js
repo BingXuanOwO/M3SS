3ac806b7243f86113e3950bbb52ea158a3eef776
BingXuanOwO <hxc35459@outlook.com>
2022-10-08 22:37:04 +0800

    修改阴影

--- m3ss.js
+++ m3ss.js
@@ -10,11 +10,12 @@ window.addEventListener('load',()=>{
 class m3ss{
     constructor(){}
     static initBtn(button){
+        //键盘触发
         button.addEventListener('click',(event)=>{
             if(event.pointerId != -1 && event.mozInputSource != 6){ return; }
             let radius = button.clientWidth > button.clientHeight ?
-                            button.clientWidth :
-                            button.clientHeight;
+                         button.clientWidth :
+                         button.clientHeight;
 
             let ripple = document.createElement('div')
             ripple.style.top = (button.clientHeight / 2) - 5 + "px";
@@ -31,31 +32,35 @@ class m3ss{
             setTimeout(()=>{ ripple.remove(); },1500)
         })
 
+        //鼠标触发
         button.addEventListener('mousedown', (e)=>{
+            //获取鼠标相对位置
             let innerx = e.clientX - button.offsetLeft;
             let innery = e.clientY - button.offsetTop;
     
-            let radius = button.clientWidth > button.clientHeight ?
-                         button.clientWidth :
-                         button.clientHeight;
-    
-            let ripple = document.createElement('div')
-            ripple.style.left  = (innerx - 5) +'px';
-            ripple.style.top = (innery - 5) + 'px';
 
+            let radius = Math.sqrt(Math.pow(button.clientWidth,2) + Math.pow(button.clientHeight,2))
+            console.log(radius)
+            //生成一个圆，作为ripple效果
+            let ripple = document.createElement('div')
             ripple.setAttribute('class','m3ss-ripple');
 
+            //设置ripple初始位置
+            ripple.style.left  = (innerx - 5) +'px';
+            ripple.style.top = (innery - 5) + 'px';
             button.appendChild(ripple);
     
-            ripple.style.top = "-" + (radius * 1.2 - button.clientHeight) / 2 + "px";
-            ripple.style.left = "-" + (radius * 1.2 - button.clientWidth) / 2 + "px";
-            ripple.style.height = radius * 1.2 + "px";
-            ripple.style.width = radius * 1.2 +"px";
+            //生成完成后，设置ripple大小至对角线，并将ripple放在中心点位置
+            ripple.style.top = "-" + (radius - button.clientHeight) / 2 + "px";
+            ripple.style.left = "-" + (radius - button.clientWidth) / 2 + "px";
+            ripple.style.height = radius + "px";
+            ripple.style.width = radius + "px";
     
+            //放开鼠标或在固定时间后逐渐将ripple效果透明度降至0
             document.onmouseup = () => { ripple.style.opacity = 0; }
-    
             setTimeout(()=>{ ripple.style.opacity = 0; },400)
     
+            //固定时间后移除ripple效果对应dom节点
             setTimeout(()=>{ ripple.remove(); },1500)
         })
     }
